071f0f4fc02bed64728767b7201d8a242f1202b6
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-05-29 14:02:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your code here
+() => {
+  cy.get('#submit') // Assuming the submit button has the ID "submit"
+    .should('have.attr', 'value', 'Submit')
+    .should('have.attr', 'type', 'submit');
+}
